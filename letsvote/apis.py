ae3e3b2d169757b8463e877589472df5c9b485a1
Gerald <gera2ld@163.com>
2016-06-15 21:03:47 +0800

    add CORS support

--- letsvote/apis.py
+++ letsvote/apis.py
@@ -14,18 +14,31 @@ def safe_get_vote(request):
         api_not_found()
     return vote
 
+def get_cors_headers(request=None):
+    if request is not None:
+        origin = request.headers.get('origin')
+    else:
+        origin = None
+    headers = {}
+    if origin is not None:
+        headers['Access-Control-Allow-Origin'] = origin
+        headers['Vary'] = 'Origin'
+    # else:
+    #     headers['Access-Control-Allow-Origin'] = '*'
+    return headers
+
 def dumps(data):
     return json.dumps(data, separators = (',', ':'), ensure_ascii = False)
 
 def api(handle):
     async def handler(request):
         data = await handle(request)
-        return aiohttp.web.json_response(data, dumps=dumps)
+        return aiohttp.web.json_response(data, dumps=dumps, headers=get_cors_headers(request))
     return handler
 
 def api_error(msg = None, error_class = aiohttp.web.HTTPServiceUnavailable,
-        data = None):
-    exc = error_class(content_type='application/json')
+        data = None, request = None):
+    exc = error_class(content_type='application/json', headers=get_cors_headers(request))
     if data is None:
         data = {
             'status': exc.status,
@@ -34,8 +47,8 @@ def api_error(msg = None, error_class = aiohttp.web.HTTPServiceUnavailable,
     exc.text = dumps(data)
     raise exc
 
-def api_not_found(msg = None):
-    api_error(msg, error_class = aiohttp.web.HTTPNotFound)
+def api_not_found(msg=None, request=None):
+    api_error(msg, error_class=aiohttp.web.HTTPNotFound, request=request)
 
 @api
 async def handle_get_detail(request):
@@ -50,7 +63,7 @@ async def handle_post_detail(request):
     try:
         vote.vote(oids)
     except AlreadyVoted:
-        api_error('Already voted!', error_class = HTTPUnprocessableEntity)
+        api_error('Already voted!', error_class=HTTPUnprocessableEntity, request=request)
     return vote.to_json()
 
 @api
@@ -67,6 +80,6 @@ async def handle_post_create(request):
     except:
         import traceback
         traceback.print_exc()
-        api_error('Invalid data!', error_class = HTTPUnprocessableEntity)
+        api_error('Invalid data!', error_class=HTTPUnprocessableEntity, request=request)
     else:
         return vote.to_json()
